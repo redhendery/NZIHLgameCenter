00b03cddd7af2a6ef8b6df3ce3a85f87d728aa4f
redhendery <redhendery@gmail.com>
2017-02-14 17:51:50 +1300

    rough complete of schedule items, able to seed and read with firebase

--- app/models/schedule.js
+++ app/models/schedule.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   home: DS.attr('string'),
   away: DS.attr('string'),
   venue: DS.attr('string'),
-  date: DS.attr('string')
+  date: DS.attr('string'),
+  game: DS.attr('string')
 });
