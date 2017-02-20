92c19224568c385a0c9bc5bf424071ddbb2e3d0e
redhendery <redhendery@gmail.com>
2017-02-20 15:18:16 +1300

    remove errors when loading schedules page

--- app/routes/schedule/index.js
+++ app/routes/schedule/index.js
@@ -2,8 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  model() {
-    return this.store.findAll('schedule');
+  queryParams: {
+    limit: { refreshModel: true, as: 'schedule' },
+    team: { refreshModel: true, as: 'other-team' },
+  },
+
+  model(params) {
+
+    if (params.limit === 'all') {
+      return this.store.findAll('schedule');
+    }
+
+    return this.store.query('schedule', {
+      orderBy: 'name',
+      startAt: params.team,
+      endAt: params.letter+"\uf8ff"
+    });
   }
 
 });
