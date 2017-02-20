88d9fbc158f4830107a0390be1c6040000760096
redhendery <redhendery@gmail.com>
2017-02-21 11:22:46 +1300

    made a few changes to get data to render but it wont. Pray for StackOverFlow

--- app/components/schedule-select.js
+++ app/components/schedule-select.js
@@ -1,4 +1,15 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Component.extend({
+
+  tagName: 'select',
+  schedule: [],
+  book: null,
+
+  change(event) {
+    const selectedScheduleId = event.target.value;
+    const selectedSchedule = this.get('schedule').find((record) => record.id === selectedScheduleId);
+
+    this.sendAction('action', selectedSchedule, this.get('schedule'));
+  }
 });
--- app/controllers/schedule.js
+++ app/controllers/schedule.js
@@ -9,22 +9,19 @@ export default Ember.Controller.extend({
 
   limitAll: Ember.computed.equal('limit', 'all'),
 
-  filteredList: Ember.computed('model.@each.name', 'filter', function() {
+  filteredList: Ember.computed('model.@each.game', 'filter', function() {
 
     let results = this.get('model');
     const query = this.get('filter');
 
     if (!!query) {
-      // One of the best regular expression website: http://www.regexr.com/
-      // Split the query at spaces and join them to get like this: /(word1)+.*(word2)+.*(word3)+.*/ig
       const regexString = '(' + query.split(' ').join(')+.*(') + ')+.*';
-      // i: case insensitive, g: global
       const regex = new RegExp(regexString, 'ig');
 
-      results = results.filter((item) => item.get('name').match(regex));
+      results = results.filter((item) => item.get('game').match(regex));
     }
 
-    return results.sortBy('name');
+    return results.sortBy('game');
   })
 
 });
