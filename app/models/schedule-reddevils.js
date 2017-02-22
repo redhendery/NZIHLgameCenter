4fb6b9ab29ca71c56531567afb3cd64deebfab04
redhendery <redhendery@gmail.com>
2017-02-22 16:28:12 +1300

    correct error where Devils schedule would not save crrectly

--- app/models/schedule-reddevils.js
+++ app/models/schedule-reddevils.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  game: DS.attr('string'),
+  home: DS.attr('string'),
+  away: DS.attr('string'),
+  venue: DS.attr('string'),
+  date: DS.attr('string'),
 });
