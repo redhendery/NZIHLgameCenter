7589f43709e2f035d4fa8d2ae23a610205a35ff1
redhendery <redhendery@gmail.com>
2017-02-22 14:53:27 +1300

    add, save, display, show for Thunder

---
+++ app/routes/schedule/thunder/new.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  model() {
+    return this.store.createRecord('scheduleThunder');
+  },
+
+  actions: {
+
+    saveScheduleThunder(newScheduleThunder) {
+      newScheduleThunder.save().then(() => this.transitionTo('schedule.thunder'));
+    },
+
+    willTransition() {
+      this.controller.get('model').rollbackAttributes();
+    }
+  }
+});
